5aaa9110cf3a693dd844d72c431379ae319cf55c
Hansanghyeon <dandan9509@gmail.com>
2020-09-12 14:56:21 +0900

    feat: 메인페이지 이력서추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import 'react-notion/src/styles.css';
 import { AppProps } from 'next/app';
+import { GridThemeProvider } from 'styled-bootstrap-grid';
+// components
+import Grid from '@style/Grid';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <GridThemeProvider gridTheme={Grid}>
+      <Component {...pageProps} />
+    </GridThemeProvider>
+  );
 }
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
 import Head from 'next/head';
-import SignIn from '@molecule/SignIn';
+import { NotionRenderer } from 'react-notion';
+import axios from 'axios';
+import { GridThemeProvider, Container, Row } from 'styled-bootstrap-grid';
+import Grid from '@style/Grid';
 
-const Home = () => {
+const Home = ({ blockMap }: any) => {
+  const _Grid = {
+    ...Grid,
+    container: {
+      maxWidth: {
+        xxl: 768,
+        xl: 768,
+        lg: 768,
+      },
+    },
+  };
   return (
     <>
       <Head>
         <title>Main Home</title>
       </Head>
-      <SignIn />
+      <GridThemeProvider gridTheme={_Grid}>
+        <Container>
+          <Row>
+            <NotionRenderer blockMap={blockMap} />
+          </Row>
+        </Container>
+      </GridThemeProvider>
     </>
   );
 };
 
 export default Home;
+export async function getStaticProps() {
+  const { data } = await axios(
+    'https://notion-api.splitbee.io/v1/page/ccd992aba32f412581339a3f5df04fe8',
+  );
+
+  return {
+    props: {
+      blockMap: data,
+    },
+  };
+}
